0b37482e1cd3f8deefa57cf5a09c64814a06079e
Matthew J. Harmon <mascon@mjh.email>
2018-01-21 00:53:31 -0600

    keyserver cannot assign requested address - reduce

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ then
 	SIGNING_ARGS="--sign"
 fi
 
-$AUTO_GPG --keyserver hkp://pool.sks-keyservers.net --recv-keys "${trustedpgpkeys[@]}"
+$AUTO_GPG --recv-keys "${trustedpgpkeys[@]}"
 for k in "${trustedpgpkeys[@]}"
 do
 	$AUTO_GPG --lsign-key "$k" || :
